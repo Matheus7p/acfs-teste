48832cedca3652d19e1483edd92c478f7d95c686
Matheus7p <matheus.costa.7p@gmail.com>
2026-02-12 20:54:20 -0300

    chore: configure eslint and fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-export default function App() {
+import { JSX } from "react";
+
+export function App (): JSX.Element {
   return (
     <>
       <main className="h-screen bg-blue-200">
         <h1>Teste tecnico</h1>
       </main>
     </>
-  )
+  );
 }
-
--- test/app.test.tsx
+++ test/app.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
-import App from "@/App";
+
+import { App } from "@/App";
 
 describe("App", () => {
   it("Teste tecnico", () => {
-   render(<App />);
-   expect(screen.getByText(/Teste tecnico/i)).toBeInTheDocument()
+    render(<App />);
+    expect(screen.getByText(/Teste tecnico/i)).toBeInTheDocument();
   });
 });
